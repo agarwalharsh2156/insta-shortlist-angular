e44c5f7a5fae177c862d5764e97d037677b648a2
Harsh Agarwal <agarwalharsh2156@gmail.com>
2025-07-16 18:18:12 +0530

    making production ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,29 +6,20 @@ import { JobsComponent } from './jobs/jobs.component';
 import { CreateJobComponent } from './jobs/create-job/create-job.component';
 import { EditJobComponent } from './jobs/edit-job/edit-job.component';
 import { AuthGuard } from './auth.guard';
-import { CandidatesComponent } from './candidates/candidates.component'
-import { CreateCandidateComponent } from './candidates/create-candidate/create-candidate.component';
-import { EditCandidateComponent } from './candidates/edit-candidate/edit-candidate.component';
-import { OffersComponent } from './offers/offers.component';
-import { ViewJobComponent } from './jobs/view-job/view-job.component';
-import { ViewCandidateComponent } from './candidates/view-candidate/view-candidate.component';
-import { ReviewFormComponent } from './candidates/review-form/review-form.component'
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'jobs', component: JobsComponent, canActivate: [AuthGuard] },
-  { path: 'candidates', component: CandidatesComponent, canActivate: [AuthGuard] },
-  { path: 'jobs/create-job', component: CreateJobComponent, canActivate: [AuthGuard] },
-  { path: 'jobs/edit-job/:id', component: EditJobComponent, canActivate: [AuthGuard] },
-  { path: 'candidates/create-candidate', component: CreateCandidateComponent, canActivate: [AuthGuard] },
-  { path: 'offers', component: OffersComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent), canActivate: [AuthGuard] },
+  { path: 'jobs', loadComponent: () => import('./jobs/jobs.component').then(m => m.JobsComponent), canActivate: [AuthGuard] },
+  { path: 'candidates', loadComponent: () => import('./candidates/candidates.component').then(m => m.CandidatesComponent), canActivate: [AuthGuard] },
+  { path: 'jobs/create-job', loadComponent: () => import('./jobs/create-job/create-job.component').then(m => m.CreateJobComponent), canActivate: [AuthGuard] },
+  { path: 'jobs/edit-job/:id', loadComponent: () => import('./jobs/edit-job/edit-job.component').then(m => m.EditJobComponent), canActivate: [AuthGuard] },
+  { path: 'candidates/create-candidate', loadComponent: () => import('./candidates/create-candidate/create-candidate.component').then(m => m.CreateCandidateComponent), canActivate: [AuthGuard] },
+  { path: 'offers', loadComponent: () => import('./offers/offers.component').then(m => m.OffersComponent), canActivate: [AuthGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'candidates', component: CandidatesComponent },
-  { path: 'candidates/create-candidate', component: CreateCandidateComponent, canActivate: [AuthGuard] },
-  { path: 'candidates/edit-candidate/:id', component: EditCandidateComponent, canActivate: [AuthGuard] },
-  { path: 'jobs/view-job/:id', component: ViewJobComponent, canActivate: [AuthGuard] },
-  { path: 'candidates/view-candidate/:id', component: ViewCandidateComponent, canActivate: [AuthGuard] },
-  { path: 'candidates/view-candidate/:id/review/:stepId', component: ReviewFormComponent, canActivate: [AuthGuard] },
+  { path: 'candidates/edit-candidate/:id', loadComponent: () => import('./candidates/edit-candidate/edit-candidate.component').then(m => m.EditCandidateComponent), canActivate: [AuthGuard] },
+  { path: 'jobs/view-job/:id', loadComponent: () => import('./jobs/view-job/view-job.component').then(m => m.ViewJobComponent), canActivate: [AuthGuard] },
+  { path: 'candidates/view-candidate/:id', loadComponent: () => import('./candidates/view-candidate/view-candidate.component').then(m => m.ViewCandidateComponent), canActivate: [AuthGuard] },
+  { path: 'candidates/view-candidate/:id/review/:stepId', loadComponent: () => import('./candidates/review-form/review-form.component').then(m => m.ReviewFormComponent), canActivate: [AuthGuard] },
 ];
